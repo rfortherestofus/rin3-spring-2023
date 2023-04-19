0512b2c3af9540ef96dfb7db5f4a9a8264d804ad
David Keyes <david@rfortherestofus.com>
2023-04-19 10:42:57 -0700

    Week 6 in-session updates

---
+++ ggtext.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(palmerpenguins)
+library(ggtext)
+
+ggplot(data = penguins,
+       aes(x = flipper_length_mm,
+           y = bill_length_mm)) +
+  geom_point() +
+  labs(title = "Penguins are *really cool*",
+       x = "Flipper Length",
+       y = "Bill Length") +
+  theme(plot.title = element_markdown())
+
+# new text
